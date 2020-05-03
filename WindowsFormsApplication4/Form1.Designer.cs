b08b649ea367bac5156c76507dba01cc061deab9
Oguzdmr <doguzhan476@gmail.com>
2020-05-03 15:32:30 +0300

    first commit

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Kale1 = new System.Windows.Forms.Label();
+            this.Kale2 = new System.Windows.Forms.Label();
+            this.Top = new System.Windows.Forms.PictureBox();
+            this.picOyuncu1 = new System.Windows.Forms.PictureBox();
+            this.picOyuncu2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerHareket = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOyuncu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOyuncu2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(467, -1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 463);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 14);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(193, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 221);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 14);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(765, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(209, 19);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(765, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 14);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(765, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 221);
+            this.label7.TabIndex = 6;
+            // 
+            // Kale1
+            // 
+            this.Kale1.BackColor = System.Drawing.Color.White;
+            this.Kale1.Location = new System.Drawing.Point(1, 111);
+            this.Kale1.Name = "Kale1";
+            this.Kale1.Size = new System.Drawing.Size(17, 221);
+            this.Kale1.TabIndex = 7;
+            // 
+            // Kale2
+            // 
+            this.Kale2.BackColor = System.Drawing.Color.White;
+            this.Kale2.Location = new System.Drawing.Point(957, 111);
+            this.Kale2.Name = "Kale2";
+            this.Kale2.Size = new System.Drawing.Size(17, 221);
+            this.Kale2.TabIndex = 8;
+            // 
+            // Top
+            // 
+            this.Top.Image = ((System.Drawing.Image)(resources.GetObject("Top.Image")));
+            this.Top.Location = new System.Drawing.Point(450, 183);
+            this.Top.Name = "Top";
+            this.Top.Size = new System.Drawing.Size(59, 50);
+            this.Top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Top.TabIndex = 9;
+            this.Top.TabStop = false;
+            this.Top.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // picOyuncu1
+            // 
+            this.picOyuncu1.Image = ((System.Drawing.Image)(resources.GetObject("picOyuncu1.Image")));
+            this.picOyuncu1.Location = new System.Drawing.Point(116, 163);
+            this.picOyuncu1.Name = "picOyuncu1";
+            this.picOyuncu1.Size = new System.Drawing.Size(71, 111);
+            this.picOyuncu1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picOyuncu1.TabIndex = 10;
+            this.picOyuncu1.TabStop = false;
+            // 
+            // picOyuncu2
+            // 
+            this.picOyuncu2.Image = ((System.Drawing.Image)(resources.GetObject("picOyuncu2.Image")));
+            this.picOyuncu2.Location = new System.Drawing.Point(851, 163);
+            this.picOyuncu2.Name = "picOyuncu2";
+            this.picOyuncu2.Size = new System.Drawing.Size(71, 111);
+            this.picOyuncu2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picOyuncu2.TabIndex = 11;
+            this.picOyuncu2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 70;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerHareket
+            // 
+            this.timerHareket.Interval = 50;
+            this.timerHareket.Tick += new System.EventHandler(this.timerHareket_Tick);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Gray;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(492, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 44);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "0";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Gray;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(411, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 44);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "0";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(972, 461);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.picOyuncu2);
+            this.Controls.Add(this.picOyuncu1);
+            this.Controls.Add(this.Top);
+            this.Controls.Add(this.Kale2);
+            this.Controls.Add(this.Kale1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kafa Topu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOyuncu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOyuncu2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Kale1;
+        private System.Windows.Forms.Label Kale2;
+        private System.Windows.Forms.PictureBox Top;
+        private System.Windows.Forms.PictureBox picOyuncu1;
+        private System.Windows.Forms.PictureBox picOyuncu2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timerHareket;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+
+        int x = 10, y = 10,l=10;
+        int i = 0,k=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void oyuncuCrpma()
+        {
+            Rectangle o1 = new Rectangle();
+            Rectangle o2 = new Rectangle();
+            Rectangle t = new Rectangle();
+
+            o1.X = picOyuncu1.Left;
+            o1.Y = picOyuncu1.Top;
+            o1.Height = picOyuncu1.Height;
+            o1.Width = picOyuncu1.Width;
+
+            o2.X = picOyuncu2.Left;
+            o2.Y = picOyuncu2.Top;
+            o2.Height = picOyuncu2.Height;
+            o2.Width = picOyuncu2.Width;
+
+            t.X = Top.Left;
+            t.Y = Top.Top;
+            t.Height = Top.Height;
+            t.Width = Top.Width;
+
+            if (t.IntersectsWith(o2))
+            {
+                if (y > 0 && x > 0)
+                {
+                    if (o2.Top <= t.Bottom && o2.Left >= t.Right)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (y > 0 && x < 0)
+                {
+                    if (o2.Top <= t.Bottom && o2.Right >= t.Left)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (y < 0 && x > 0)
+                {
+                    if (o2.Bottom >= t.Top && o2.Left >=t.Right)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    if (o2.Bottom <= t.Top && o2.Right >= t.Left)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+
+            }
+
+            else if (t.IntersectsWith(o1))
+            {
+                if (y > 0 && x > 0)
+                {
+                    if (o1.Top <= t.Bottom && o1.Left >= t.Right)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (y > 0 && x < 0)
+                {
+                    if (o1.Top <= t.Bottom && o1.Right >= t.Left)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (y < 0 && x > 0)
+                {
+                    if (o1.Bottom >= t.Top && o1.Left >= t.Right)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+                else if (x < 0 && y < 0)
+                {
+                    if (o1.Bottom <= t.Top && o1.Right >= t.Left)
+                        y = y * -1;
+                    else
+                        x = x * -1;
+                }
+            }
+
+        }
+
+        private void carpma()
+        {
+            Rectangle k1 = new Rectangle();
+            Rectangle k2 = new Rectangle();
+            Rectangle t = new Rectangle();
+
+            k1.X = Kale1.Left;
+            k1.Y = Kale1.Top;
+            k1.Height = Kale1.Height;
+            k1.Width = Kale1.Width;
+
+            k2.X = Kale2.Left;
+            k2.Y = Kale2.Top;
+            k2.Height = Kale2.Height;
+            k2.Width = Kale2.Width;
+
+            t.X = Top.Left;
+            t.Y = Top.Top;
+            t.Height = Top.Height;
+            t.Width = Top.Width;
+
+            if (t.IntersectsWith(k1))
+            {
+                k++;
+                label8.Text = Convert.ToString(k);
+                Top.Location = new Point(446, 177);
+                
+            }
+
+            if (t.IntersectsWith(k2))
+            {
+                i++;
+                label9.Text = Convert.ToString(i);
+                Top.Location = new Point(446, 177);
+                
+            }
+
+        }
+
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+           
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Top.Left += x;
+            Top.Top += y;
+
+            if (Top.Left > this.Width - Top.Width)
+            {
+                x = x * -1;
+
+            }
+            if (Top.Left < 0)
+            {
+                x = x * -1;
+            }
+            if (Top.Top > this.Height - Top.Height)
+            {
+                y = y * -1;
+            }
+            if (Top.Top < 0)
+            {
+                y = y * -1;
+            }
+
+
+            oyuncuCrpma();
+            carpma();
+
+        }
+
+    
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+          
+            if (e.KeyCode == Keys.Up)
+            {
+                picOyuncu2.Top -= l;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                picOyuncu2.Top += l;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                picOyuncu2.Left += l;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                picOyuncu2.Left -= l;
+            }
+            
+        }
+
+        private void timerHareket_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==119 || e.KeyChar==87)
+            picOyuncu1.Top -= l;
+
+            if (e.KeyChar == 115 || e.KeyChar == 83)
+                picOyuncu1.Top += l;
+
+            if (e.KeyChar == 100 || e.KeyChar == 68)
+                picOyuncu1.Left += l;
+
+            if (e.KeyChar == 97 || e.KeyChar == 65)
+                picOyuncu1.Left -= l;
+        }
+    }
+}
